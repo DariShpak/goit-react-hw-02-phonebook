3159d591b73b8bb6f65350ac47c2a9deb1321f7a
Daria Shpak <darya.shpak@gmail.com>
2023-07-04 03:54:21 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -66,11 +66,3 @@ class App extends Component {
 
 export default App
 
-// <div>
-//   <h1>Phonebook</h1>
-//   <ContactForm ... />
-
-//   <h2>Contacts</h2>
-//   <Filter ... />
-//   <ContactList ... />
-// </div>
