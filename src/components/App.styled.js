31b6871d2817c6b7de0151c31ace06dca2600ecf
Daria Shpak <darya.shpak@gmail.com>
2023-07-04 03:36:07 +0300

    Update App.styled.js

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -12,11 +12,7 @@ export const MainTitle = styled.h1`
   color: #740001;
 `
 
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-`
+
 export const Label = styled.label`
   display: flex;
   justify-content: space-between;
